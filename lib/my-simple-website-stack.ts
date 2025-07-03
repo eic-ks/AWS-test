aa8de44771346eb70b94a17e3b5af408ba6d7e7f
eic-ks at cloud shell <noreply@example.com>
2025-07-03 08:53:53 +0000

    シンプルなwebサイトの作成完了

--- lib/my-simple-website-stack.ts
+++ lib/my-simple-website-stack.ts
@@ -1,9 +1,11 @@
-// v2では'aws-cdk-lib'から必要なものをまとめてインポートします
+// lib/my-simple-website-stack.ts
+
 import { Stack, StackProps, CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
-import { aws_s3 as s3 } from 'aws-cdk-lib';
-import { aws_s3_deployment as s3deploy } from 'aws-cdk-lib';
-import { aws_cloudfront as cloudfront } from 'aws-cdk-lib';
-import { aws_cloudfront_origins as origins } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+// ↓↓↓ この行を修正しました
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Construct } from 'constructs';
 
 export class MySimpleWebsiteStack extends Stack {
@@ -12,23 +14,28 @@ export class MySimpleWebsiteStack extends Stack {
 
     // 1. ウェブサイトのファイルを格納するS3バケットを作成
     const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
-      websiteIndexDocument: 'index.html',
-      publicReadAccess: true,
-      removalPolicy: RemovalPolicy.DESTROY, 
+      removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
+
+    // Origin Access Identity (OAI) を作成
+    // ↓↓↓ 'new cloudfront.OriginAccessIdentity' の形に修正しました
+    const oai = new cloudfront.OriginAccessIdentity(this, 'MyWebsiteOAI');
+    websiteBucket.grantRead(oai);
 
     // 2. CloudFrontディストリビューションを作成
     const distribution = new cloudfront.Distribution(this, 'MyDistribution', {
       defaultBehavior: {
-        origin: new origins.S3Origin(websiteBucket),
+        origin: new origins.S3Origin(websiteBucket, {
+          originAccessIdentity: oai,
+        }),
       },
       defaultRootObject: 'index.html',
     });
 
-    // 3. 'website' ディレクトリのファイルをS3バケットにデプロイ
+    // 3. 'frontend' ディレクトリのファイルをS3バケットにデプロイ
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-      sources: [s3deploy.Source.asset('./website')],
+      sources: [s3deploy.Source.asset('./frontend')],
       destinationBucket: websiteBucket,
       distribution: distribution,
       distributionPaths: ['/*'],
